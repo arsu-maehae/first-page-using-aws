9b4a2f8aac479c3f1835c06dcd7f61994cc5ea92
arsumaehae <arsumaehae@gmail.com>
2026-02-13 22:07:31 +0700

    from arsu-maehae to arsu1010

--- superlists/test_selenium.py
+++
@@ -1,9 +0,0 @@
-from selenium import webdriver
-
-# Selenium Manager automatically finds and downloads the driver for you
-driver = webdriver.Chrome()
-
-driver.get("https://docs.djangoproject.com/en/6.0/intro/tutorial01/")
-print(driver.title)
-assert("part 1" in driver.title)
-driver.quit()
